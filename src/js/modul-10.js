2e3a1b4a6c00a368cb39338e0ed0f5efded3ba13
AllaP47 <allapaschinska1@gmail.com>
2025-03-09 16:37:43 +0200

    add message

--- src/js/modul-10.js
+++ src/js/modul-10.js
@@ -7,3 +7,34 @@ setInterval(() => {
     timeFormEl.textContent = new Date().toLocaleString();
 }, 1000);
 
+// - Використовуй prompt та повертай значення звідти.
+// - Створи функцію, яка буде набувати значення з prompt і повертатиме проміс.
+// Якщо значення не є числом, відхиляй проміс та логіруй "error".
+// Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
+// Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
+
+const answer = prompt('Enter something');
+function checkAnswer(answer) {
+    return new Promise((resolve, reject) => {
+        const answerNum = Number(answer);
+        if (isNaN(answerNum)) {
+            reject("error");
+        } 
+        if (answerNum % 2 === 0) {
+            setTimeout(() => {resolve("even")}, 1000);
+
+        }
+        if (answerNum % 2 !== 0) {
+            setTimeout(() => { resolve("odd") }, 2000);
+        }
+
+ })
+}
+checkAnswer(answer).then((data) => {
+    console.log(data);
+    
+}).catch((err) => {
+    console.log(err);
+    
+})
+
