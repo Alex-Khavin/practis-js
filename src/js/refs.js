e21a2273ce273362a81acceb4801b33a6d487d00
Alex Khavin <alexandr_havin@yahoo.com>
2025-03-09 15:14:11 +0200

    add form

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+    form: document.querySelector(".login-form"),
+    inputEmail: document.querySelector("[name='email']"),
+    inputPass: document.querySelector("[name='password']"),
+    btn: document.querySelector("[name='button']")
+}
---
+++ src/main.js
@@ -0,0 +1,50 @@
+// Перед вами форма для авторизації на сайті (правильні дані для входу збережені в обʼєкті USER_DATA)
+
+// Розбийте код на кілька файлів:
+// storage.js - функції для роботи зі сховищем;
+// refs.js - посилання на всі потрібні елементи в html;
+// main.js - головний файл, де вся основна логіка додатка.
+// Ви маєте додати перевірку введених даних при сабміті:
+// Якщо введені дані не збігаються зі збереженими, викликайте аlert і
+// повідомляйте про помилку.
+
+// Якщо імейл і пароль користувача збігаються, зберігайте дані з форми
+// у локальне сховище і змінюйте кнопку Login на Logout, також робіть поля введення
+// недоступними для змін.
+// При перезавантаженні сторінки, якщо користувач авторизован, ми маємо бачити logout-кнопку
+// та недоступні для зміни поля з даними користувача.
+// Клік по кнопці logout повертає все до початкового вигляду і видаляє дані користувача
+// з локального сховища.
+
+import { refs } from "./js/refs";
+import { saveData } from "./js/storage";
+
+const USER_DATA = {
+  email: 'user@mail.com',
+  password: 'secret',
+};
+
+const LS_KEY = "user-data";
+
+refs.form.addEventListener("submit", handlerSubmit)
+
+function handlerSubmit(event) {
+    event.preventDefault();
+    const emailValue = refs.inputEmail.value.trim();
+    const passValue = refs.inputPass.value.trim();
+
+    if (emailValue === "" || passValue === "") {
+        alert("Заповніть усі поля!")
+        return;
+    }
+    if (emailValue !== USER_DATA.email || passValue !== USER_DATA.password) {
+        alert("Данні не коректні")
+        return;
+    }
+    saveData(LS_KEY, { email: emailValue, password: passValue })
+    refs.btn.textContent = "Logout";
+    refs.inputEmail.setAttribute("readonly", true);
+    refs.inputPass.setAttribute("readonly", true);
+
+}
+
