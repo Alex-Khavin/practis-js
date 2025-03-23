5015ec701e65f3860c038cca822d3021ae67dc62
Sasha <malinka15117kk@gmail.com>
2025-03-23 15:20:59 +0200

    add new method

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -3,10 +3,26 @@ import axios from 'axios';
 export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+  #query = "";
+
   async getPopularPhotos(page) {
     const url = `${this.#BASE_URL}?client_id=${
       this.#API_KEY
     }&query=popular&page=${page}&per_page=12&orientation=portrait`;
+    try {
+      const { data } = await axios(url);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  set query(newQuery) {
+    this.#query = newQuery;
+  }
+  async getPhotosByQuery(page) {
+    const url = `${this.#BASE_URL}?client_id=${
+      this.#API_KEY
+    }&query=${this.#query}&page=${page}&per_page=12&orientation=portrait`;
     try {
       const { data } = await axios(url);
       return data;
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -11,10 +11,29 @@
 // // Додай слухача на форму, щоб робити новий запит по ключовому слову (додавши відповідний метод класу UnsplashAPI).
 // // Додай лоадер під час завантаження даних з бекенда.
 // // Не забудь про відповідні перевірки і сповіщення при роботі з запитами і з формою.
+import "tui-pagination/dist/tui-pagination.min.css"
+import Pagination from 'tui-pagination';
 import { renderGallery } from './createGalleryCard';
 import { UnsplashAPI } from './UnsplashAPI';
+
 const api = new UnsplashAPI();
 
-api.getPopularPhotos(1).then(({ results }) => {
+const container = document.getElementById('tui-pagination-container');
+const pagination = new Pagination(container, {
+  totalItems: 0,
+     itemsPerPage: 12,
+     visiblePages: 5,
+     page: 1, });
+const page = pagination.getCurrentPage();
+
+api.getPopularPhotos(page).then(({ results, total }) => {
   renderGallery(results);
+  pagination.reset(total);
+});
+
+pagination.on('afterMove', (event) => {
+     const currentPage = event.page;
+     api.getPopularPhotos(currentPage).then(({ results }) => {
+  renderGallery(results);
+});
 });
